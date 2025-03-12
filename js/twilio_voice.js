35d4496e44e0a093fa7cb445e1da6ad3a8f0c43b
annevjenkins22a <annevjenkins22@gmail.com>
2025-03-12 15:56:44 +0100

    Update twilio_voice.js

--- js/twilio_voice.js
+++ js/twilio_voice.js
@@ -433,7 +433,8 @@ const inputHandler = this.InputHandler();
                             action: postPath.default,
                             actionOnEmptyResult: false,
                             language: twilioLanguage,
-                            timeout: 2,
+                            timeout: 5,
+                            speechTimeout: 'auto',
                             speechModel: "googlev2_telephony",
                             hints: hintText
                         }).say({
