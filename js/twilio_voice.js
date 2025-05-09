fcc0802978e94582e6dd74033bfc6ba23963b372
AnneJ <73593442+annevjenkins42@users.noreply.github.com>
2025-05-09 12:06:49 +0200

    Update twilio_voice.js

--- js/twilio_voice.js
+++ js/twilio_voice.js
@@ -662,6 +662,9 @@ const inputHandler = this.InputHandler();
                     console.log("Content to Teneo: " + JSON.stringify(contentToTeneo).toString());
                     // Add "_phone" to as key to session to make each session, regardless when using call/sms
                     teneoResponse = await teneoApi.sendInput(teneoSessionId, contentToTeneo);
+                    if(teneoResponse == null || teneoResponse.output==null || teneoResponse.output.text==null) {
+                        teneoResponse = await teneoApi.sendInput(teneoSessionId, contentToTeneo);
+                    }
                      teneoSessionId = teneoResponse.sessionId;
                     console.log("session ID retrieved3: " + teneoSessionId);
                      userInput = teneoResponse.output.text;
